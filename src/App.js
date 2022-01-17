badf503200668473498b3c28faf4a7c54aa5be86
joon <83916101+jackma914@users.noreply.github.com>
2022-01-17 21:53:13 +0900

    22-01-17
    react-route-dom v5에서의 라우트를 공부하면서 v6에 적용하는중입니다.
    
    v6에서 한 path에 2개 컴포넌트를 넣는 방법을 몰라서 오래 걸렸습니다.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
+    <div className="App">
+      <BrowserRouter>
+        {/* <Routes>
           <Route path="/" element={<Header />} />
+        </Routes>
+        <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-      </div>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/checkout" element={<Header />} />
+        </Routes>
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes> */}
+
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
