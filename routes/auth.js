5c47c41671f824208e21f53e10763ba1db21962a
joon <83916101+jackma914@users.noreply.github.com>
2022-02-12 20:52:01 +0900

    ADD User validation
    사용자가 이미 있는지를 확인합니다.
    email, password, name, confirmPassword를 validator을 이용하여 형식을 만들었습니다.

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const validateRegisterInput = require("../validation/registerValidation");
 
 // @route    GET  /api/auth/test
 // @desc     Test the auth route
@@ -15,6 +16,24 @@ router.get("/test", (req, res) => {
 // @access   Public
 router.post("/register", async (req, res) => {
   try {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    // console.log(isValid);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    // check for existing user, 유저가 있는지 여부 확인, 정규 표현식을 사용해서 검색합니다.
+    const existingEmail = await User.findOne({
+      email: new RegExp("^" + req.body.email + "$", "i"),
+    });
+
+    if (existingEmail) {
+      return res
+        .status(400)
+        .json({ error: "there is already a user with this email" });
+    }
+
     //hash the password
     const hashedPassword = await bcrypt.hash(req.body.password, 12);
 
