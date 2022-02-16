abfe2a5a7f0f4551244a89e799d90d555ada70f8
joon <83916101+jackma914@users.noreply.github.com>
2022-02-16 20:54:33 +0900

    ADD
    ADD

--- client/src/components/AuthBox.js
+++ client/src/components/AuthBox.js
@@ -5,6 +5,7 @@ function AuthBox({ register }) {
     <div className="auth">
       <div className="auth__box">
         <div className="auth__header">
+<<<<<<< HEAD
           <h1>Login</h1>
 
           <h1>{true ? "Register" : "Login"}</h1>
@@ -15,10 +16,24 @@ function AuthBox({ register }) {
 
       <form>
         {register && (
+=======
+          <h1>{true ? "Register" : "Login"}</h1>
+        </div>
+
+        <form>
+          {register && (
+            <div className="auth__field">
+              <label> Name</label>
+              <input type="text" />
+            </div>
+          )}
+
+>>>>>>> parent of ba76c49 (Revert "ADD React Context API")
           <div className="auth__field">
             <label> Name</label>
             <input type="text" />
           </div>
+<<<<<<< HEAD
         )}
 
         <div className="auth__field">
@@ -32,11 +47,29 @@ function AuthBox({ register }) {
         </div>
 
         {register && (
+=======
+
+>>>>>>> parent of ba76c49 (Revert "ADD React Context API")
           <div className="auth__field">
             <label>Confirm Password</label>
             <input type="password" />
 
+<<<<<<< HEAD
             {/* <p className="auth__error">Something went wrong</p> */}
+=======
+          {register && (
+            <div className="auth__field">
+              <label>Confirm Password</label>
+              <input type="password" />
+
+              {/* <p className="auth__error">Something went wrong</p> */}
+            </div>
+          )}
+
+          <div className="auth__footer">
+            <p className="auth__error">Something went wrong</p>
+            <button className="btn">{register ? "Register" : "Login"}</button>
+>>>>>>> parent of ba76c49 (Revert "ADD React Context API")
           </div>
         )}
 
