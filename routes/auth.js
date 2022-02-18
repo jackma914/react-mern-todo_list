ff4d7cce1d90c2115eb4331642e517c14be1f4e8
joon <83916101+jackma914@users.noreply.github.com>
2022-02-18 22:04:46 +0900

    ADD User validation
    기존 유저 유효성(중복) 검사,
    유효성 검사 모듈을 구현후 /register로 넘어오는 데이터를 유효성 검사후 error를 반환합니다.

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
+const validateRegisterInput = require("../validation/registerValidation");
+
 //비밀번호 암호화
 const bcrypt = require("bcryptjs");
 
@@ -16,9 +18,31 @@ router.get("/test", (req, res) => {
 // @desc    Create a new user
 // @access  Public
 router.post("/register", async (req, res) => {
-  // 비밀번호 암호화
-  const hashedPassword = await bcrypt.hash(req.body.password, 12);
   try {
+    // 유효성 검사 모듈을 가져와서 받아온 데이터를 전달합니다.
+    const { errors, isVlid } = validateRegisterInput(req.body);
+    console.log(`유효성 : ${isVlid} `);
+
+    if (!isVlid) {
+      return res.status(400).json(errors);
+    }
+
+    // 유저 중복 확인
+    // 아래 두가지 중복확인 변수가 있습니다. 첫번째 변수를 사용하면 로그인할때 첫글자를 대문자로하면 중복이 아닌걸로 됩니다. 이를 방지하기위해
+    // 정규 표현실을 사용하여 같은 스펠링이라면 첫글자가 대문자인걸 무시하도록 구현했습니다.
+    // const existingEmail = await User.findOne({ email: req.body.email });
+    const existingEmail = await User.findOne({
+      email: new RegExp("^" + req.body.email + "$", "i"),
+    });
+
+    if (existingEmail) {
+      return res
+        .status(400)
+        .json({ err: "There is already a user with this email" });
+    }
+
+    // 비밀번호 암호화
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
     // new user 생성
     const newUser = new User({
       email: req.body.email,
---
+++ validation/registerValidation.js
@@ -0,0 +1,52 @@
+const Validator = require("validator");
+const isEmpty = require("./inEmpty");
+
+const validateRegisterInput = (data) => {
+  let errors = {};
+
+  // 이메일 검사 필드
+  // 이메일 칸이 비었는지를 검사하는 imEmpty를 이용해 검사합니다.
+  if (isEmpty(data.email)) {
+    errors.email = "Email field can not be empty";
+
+    // Validator의 isEmail 메서드를 이용해서 이메일 양식을 검사합니다.
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid, please provide a valid email";
+  }
+
+  // 이름 검사 필드
+  if (isEmpty(data.name)) {
+    errors.password = "Name field can not be empty";
+
+    // Validater의 isLength 메서드를 이용해 비밀번호의 최소,최대 숫자수를 정해서 검사할수 있습니다.
+  } else if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = "Name must be between 2 and 30 characters long";
+  }
+
+  // 비밀번호 검사 필드
+  if (isEmpty(data.password)) {
+    errors.password = "Password field can not be empty";
+
+    // Validater의 isLength 메서드를 이용해 비밀번호의 최소,최대 숫자수를 정해서 검사할수 있습니다.
+  } else if (!Validator.isLength(data.password, { min: 6, max: 150 })) {
+    errors.password = "Password must be between 6 and 150 characters long";
+  }
+
+  // 비밀번호 확인 필드
+  if (isEmpty(data.confirmPassword)) {
+    errors.confirmPassword = "Confirm Password field can not be empty";
+
+    //Validator의 equals를 이용해서 비밀번호와 비밀번호 확인 필드를 비교해서 검사합니다.
+  } else if (!Validator.equals(data.password, data.confirmPassword)) {
+    errors.confirmPassword = "Password and Confirm Password fields must match";
+  }
+
+  return {
+    errors,
+
+    // isEmpty가 비어있으면 true를 반환합니다.
+    isVlid: isEmpty(errors),
+  };
+};
+
+module.exports = validateRegisterInput;
