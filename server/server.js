c72212bfa9aeb0db81330cbbed7a8b94a2fe21e0
Junhyeok Sim <83916101+jackma914@users.noreply.github.com>
2022-03-31 01:17:18 +0900

    ADD

--- server/server.js
+++ server/server.js
@@ -29,11 +29,11 @@ app.post("/name", (req, res) => {
 app.use("/api/auth", authRoute);
 app.use("/api/todos", toDosRoute);
 
-// app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
